ed42ffbb668b19908121cd6d6666efe6bb92d2d1
povedan0 <guilherme.povedano@estudiante.uam.es>
2025-03-19 20:02:42 +0100

    Created initial tests for link module, doc pending

---
+++ include/link_test.h
@@ -0,0 +1,222 @@
+/**
+ * @brief it defines all tests for the link module 
+ * 
+ * @file link_test.h
+ * @author Guilherme Povedano 
+ * @version 0.0.1
+ * @date 19-03-2025
+ * @copyright GNU Public License 
+*/
+#ifndef LINK_TEST_H
+#define LINK_TEST_H
+
+#include "link.h"
+#include "types.h"
+#include "test.h"
+
+
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_create(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_create(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test03_link_create(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_set_origin_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_set_origin_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test03_link_set_origin_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test04_link_set_origin_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_set_destination_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_set_destination_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test03_link_set_destination_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test04_link_set_destination_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_set_direction(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_set_direction(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test03_link_set_direction(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test04_link_set_direction(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_set_is_open(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_set_is_open(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test03_link_set_is_open(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test04_link_set_is_open(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_get_is_open(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_get_is_open(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_get_direction(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_get_direction(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_get_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_get_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_get_origin_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test02_link_get_origin_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+
+void test01_link_get_destination_id(void);
+/**
+ * @brief 
+ * @pre 
+ * @post
+*/
+void test02_link_get_destination_id(void);
+
+
+#endif
