712704ab4a45d7431bf0c6a33de7387516621d49
araceli0819 <araceligllorente@gmail.com>
2025-03-23 12:11:02 +0100

    module Player updated to include backpack

---
+++ src/inventory.c
@@ -0,0 +1,236 @@
+/**
+ * @brief It implements the inventory module
+ *
+ * @file inventory.c
+ * @author PPROG - Equipo 2101_D - AGL
+ * @version 2.0.7
+ * @date 27-01-2025
+ * @copyright GNU Public License
+ */
+
+ #include "../include/inventory.h"
+  #include "../include/set.h"
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
+/**
+* @brief inventory.c
+*
+* This struct stores all the information of an inventory.
+*/
+struct _Inventory {
+  Set *objs;               /*!< Set of objects in the player's backpack */
+  int max_objs;             /*!< max number of objects in the player's backpack */
+};
+ 
+  /** This function creates a new inventory with a given maximum number of objects  */
+  Inventory* inventory_create(int max_objs) {
+    Inventory* new_inventory = NULL;
+
+    /*ensure valid input*/
+    if(max_objs < 0){
+      return NULL;
+    }
+ 
+    /*allocate for the new inventory and if allocation successful*/
+    new_inventory = (Inventory*)malloc(sizeof(Inventory));
+    if (new_inventory == NULL) {
+      return NULL;
+    }
+
+    /*Initialize the set of objects*/
+    new_inventory-> objs = set_create();  
+    if(new_inventory-> objs == NULL){
+      inventory_destroy(new_inventory);
+      return NULL;
+    }
+
+    /*set the maximum number of objects*/
+    new_inventory->max_objs = max_objs;
+ 
+    return new_inventory;
+  }
+ 
+  /** This function destroys an inventory and frees the memory allocated for
+   * the Inventory structure and its associated Set of objects  */
+  Status inventory_destroy(Inventory* inventory) {
+    if (!inventory) {
+      return ERROR;
+    }
+ 
+    if(inventory->objs != NULL){
+      set_destroy(inventory->objs);
+    }
+
+    free(inventory);
+    return OK;
+  }
+ 
+  /**This function returns a pointer to the set of objects contained in the 
+   * inventory. If the inventory pointer is NULL, it returns NULL. */
+  Set* inventory_get_objs(Inventory* inventory) {
+    if (!inventory) {
+      return NULL;
+    }
+    return inventory->objs;
+  }
+
+  /**This function returns the maximum number of objects that the inventory can
+  * hold. If the inventory pointer is NULL, it returns -1. */
+  int inventory_get_max_objs(Inventory* inventory) {
+    if (!inventory) {
+      return -1;  
+    }
+    return inventory->max_objs;
+  }
+
+  /** This function checks if the inventory contains a specific object by
+  * using the set_belongs function to check for the presence of the object ID in the set */
+  Bool inventory_contains(Inventory *inventory, Id object_id) {
+    if (!inventory || !inventory->objs || object_id == NO_ID) {
+      return FALSE;
+    }
+  
+    if(set_belongs(inventory->objs, object_id) == FALSE){
+      return FALSE;
+    }
+  
+    return TRUE;
+  }
+
+  /*** This function sets the set of objects contained in the inventory. It updates
+  * the inventory's set of objects to the given set. If either the inventory
+  * pointer or the set pointer is NULL, it returns ERROR. */
+  Status inventory_set_objs(Inventory* inventory, Set* objs) {
+    if (!inventory || !objs) {
+      return ERROR;
+    }
+    inventory->objs = objs;
+    return OK;
+  }
+
+  /**This function sets the maximum number of objects that the inventory can hold
+  * to the value passed as argument. If the inventory pointer is NULL or if max_objs is
+  * negative, it returns ERROR. */
+  Status inventory_set_max_objs(Inventory* inventory, int max_objs) {
+    if (!inventory || max_objs < 0) {
+      return ERROR;
+    }
+    inventory->max_objs = max_objs;
+    return OK;
+  }
+
+  /**This function checks if the inventory has reached its maximum capacity by
+  * using the set_get_num_elements function to get the number of elements in the set. */
+  Bool inventory_is_full(Inventory* inventory) {
+
+    int num_objects = 0;
+
+    if (!inventory) {
+      return FALSE;
+    }
+
+   num_objects = set_get_num_elements(inventory->objs);
+    if (num_objects == -1) {
+      return FALSE;
+    }
+    return num_objects >= inventory->max_objs;
+  }
+
+  /**This function checks if the inventory is empty by using the set_get_num_elements
+  * function to get the number of elements in the set. */
+  Bool inventory_is_empty(Inventory *inventory){
+
+    int num_objects = 0;
+
+    if (!inventory) {
+      return TRUE; 
+    }
+
+    num_objects = set_get_num_elements(inventory->objs);
+    if(num_objects == 0 || num_objects == -1){
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**This function returns the object ID at the specified position in the inventory. */
+  Id inventory_get_object_id_at(Inventory *inventory, int position){
+    if (inventory == NULL || position < 0 || position >= set_get_num_elements(inventory->objs)){
+      return NO_ID;
+    }
+
+    return set_get_Id_in_pos(inventory->objs, position);
+  }
+
+  /**This function adds an object to the inventory if there is space available. */
+  Status inventory_add_object_id(Inventory* inventory, Id obj_id) {
+    if (inventory == NULL || obj_id == NO_ID) {
+      return ERROR;
+    }
+
+    if (inventory_is_full(inventory) == TRUE) {
+      return ERROR;
+    }
+
+    if(set_add(inventory->objs, obj_id) == ERROR){
+      return ERROR;
+    }
+
+    return OK;
+  }
+
+  /** This function removes an object from the inventory */
+  Status inventory_del_object_id(Inventory* inventory, Id obj_id) {
+    if (!inventory || obj_id == NO_ID) {
+      return ERROR;
+    }
+
+    /*set_del checks first if the object belogs to the set, and then removes it*/
+    if(set_del(inventory->objs, obj_id) == ERROR){
+      return ERROR;
+    }
+
+    return OK;
+  }
+
+  /**This function prints the details of the inventory, including the maximum
+ * number of objects and the IDs of the objects currently in the inventory. */
+  Status inventory_print(Inventory *inventory) {
+
+    int i;
+    int num_objects = -1;
+    Id obj_id_aux = NO_ID;
+
+    if (!inventory) {
+      return ERROR;
+    }
+
+    fprintf(stdout, "Inventory:\n");
+    fprintf(stdout, "Max objects: %d\n", inventory->max_objs);
+
+    num_objects = set_get_num_elements(inventory->objs);
+    if (num_objects != -1) {
+      fprintf(stdout, "Number of objects: %d\n", num_objects);
+      /*Print ID of every object in the inventory*/
+      fprintf(stdout, "Object IDs: ");
+      for (i = 0; i < num_objects; i++) {
+        obj_id_aux = set_get_Id_in_pos(inventory->objs, i);
+        if (obj_id_aux != NO_ID) {
+          fprintf(stdout, "%ld ", obj_id_aux);
+        }
+      }
+    }else{
+      fprintf(stdout, "No objects in the inventary");
+    }
+  
+    fprintf(stdout, "\n");
+
+    return OK;
+  }
+ 
+ 
+ 
--- src/player.c
+++ src/player.c
@@ -2,34 +2,35 @@
  * @brief It implements the player module
  *
  * @file player.c
- * @author Matteo Artuñedo
+ * @author Matteo Artuñedo, AGL (modifications to include backpack)
  * @version 0
- * @date 10-02-2025
+ * @date 22-03-2025
  * @copyright GNU Public License
  */
 
-#include "player.h"
+#include "../include/player.h"
+#include "../include/inventory.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
  * @brief Player
- * @author Matteo Artunedo
- * @date 12-02-2025
+ * @author PPROG - Team 2101_D - Matteo Artunedo, AGL (modifications to include backpack)
+ * @date 22-03-2025
  * This struct stores all the information of a player.
  */
 struct _Player
 {
-  Id id;                    /*!< Id number of the player, it must be unique */
-  char name[WORD_SIZE + 1]; /*!< Name of the player */
-  Id location;              /*!<Id of the space where the player is located*/
-  Id object;                /*!< The Id of the object the player is carrying */
-  int health;               /*!< Hp points of the player*/
+  Id id;                              /*!< Id number of the player, it must be unique */
+  char name[WORD_SIZE + 1];           /*!< Name of the player */
+  Id location;                        /*!<Id of the space where the player is located*/
+  Inventory *backpack;                /*!< Backpack to carry multiple objects */
+  int health;                         /*!< Hp points of the player*/
 };
 
-Player *player_create(Id id)
-{
+/**This function creates a new player with the given ID and initializes its fields. */
+Player *player_create(Id id){
   Player *newPlayer = NULL;
 
   /* Error control */
@@ -37,8 +38,7 @@ Player *player_create(Id id)
     return NULL;
 
   newPlayer = (Player *)malloc(sizeof(Player));
-  if (newPlayer == NULL)
-  {
+  if (newPlayer == NULL){
     return NULL;
   }
 
@@ -46,137 +46,170 @@ Player *player_create(Id id)
   newPlayer->id = id;
   newPlayer->name[0] = '\0';
   newPlayer->location = NO_ID;
-  newPlayer->object = NO_ID;
+  newPlayer->backpack = inventory_create(5); /* Initialize backpack with a capacity of 5 objects */
   newPlayer->health = 5;
 
   return newPlayer;
 }
 
-Status player_destroy(Player *player)
-{
-  if (!player)
-  {
+/** This function destroys a player and frees the allocated memory */
+Status player_destroy(Player *player){
+  if (!player){
     return ERROR;
   }
 
+  inventory_destroy(player->backpack); /* Destroy the backpack */
   free(player);
   return OK;
 }
 
-Status player_set_id(Player *player, Id playerId)
-{
-  if (!player || !playerId)
-  {
+/**This function sets the ID of the player */
+Status player_set_id(Player *player, Id playerId){
+  if (!player || !playerId){
     return ERROR;
   }
   player->id = playerId;
   return OK;
 }
 
-Id player_get_id(Player *player)
-{
-  if (!player)
-  {
+/** Gets the ID of the player */
+Id player_get_id(Player *player){
+  if (!player){
     return NO_ID;
   }
   return player->id;
 }
 
-Status player_set_name(Player *player, char *name)
-{
-  if (!player || !name)
-  {
+/**Sets the name of the player */
+Status player_set_name(Player *player, char *name){
+  if (!player || !name){
     return ERROR;
   }
 
-  if (!strcpy(player->name, name))
-  {
+  if (!strcpy(player->name, name)){
     return ERROR;
   }
   return OK;
 }
 
-const char *player_get_name(Player *player)
-{
-  if (!player)
-  {
+/** This function returns the name of the player */
+const char *player_get_name(Player *player){
+  if (!player){
     return NULL;
   }
   return player->name;
 }
 
-Status player_set_object(Player *player, Id objectId)
-{
-  if (!player)
-  {
+/**This function adds an object to the player's backpack if the player and the backpack are valid.
+ * It uses the inventory_add_object_id function to add the object to the backpack. */
+Status player_add_object_to_backpack(Player *player, Id obj_id){
+  if (player ==NULL || player->backpack == NULL){
     return ERROR;
   }
-  player->object = objectId;
+  
+  if(inventory_add_object_id(player->backpack, obj_id) == ERROR){
+    return ERROR;
+  }
+
   return OK;
 }
 
-Id player_get_object(Player *player)
-{
-  if (!player)
-  {
-    return NO_ID;
+/**This function removes an object from the player's backpack by using the inventory_del_object_id function. */
+Status player_remove_object_from_backpack(Player *player, Id obj_id){
+  if (player == NULL || player->backpack == NULL){
+    return ERROR;
   }
-  return player->object;
+
+  if(inventory_del_object_id(player->backpack, obj_id) == ERROR){
+    return ERROR;
+  }
+
+  return OK;
 }
 
-Status player_set_location(Player *player, Id spaceId)
-{
-  if (!player)
-  {
+/** This function returns the number of objects in the player's backpack */
+int player_get_num_objects_in_backpack(Player *player){
+  int n= -1;
+
+  if (player == NULL || player->backpack == NULL){
+    return -1;
+  }
+
+  n= set_get_num_elements(inventory_get_objs(player ->backpack));
+
+  return n;
+}
+
+/**This function returns the object ID at the specified position in the player's backpack*/
+Id player_get_backpack_object_id_at(Player *player, int position){
+  if (player == NULL || player->backpack == NULL || position < 0 || position >=  player_get_num_objects_in_backpack(player)){
+    return NO_ID;
+  }
+
+  return inventory_get_object_id_at(player->backpack, position);
+}
+
+/** This function checks if the player's backpack contains the specified object ID */
+Bool player_backpack_contains(Player *player, Id obj_id){
+  if (!player || !player->backpack){
+    return FALSE;
+  }
+  return inventory_contains(player->backpack, obj_id);
+}
+
+/**This function sets the location of the player */
+Status player_set_location(Player *player, Id spaceId){
+  if (!player){
     return ERROR;
   }
   player->location = spaceId;
   return OK;
 }
 
-Id player_get_location(Player *player)
-{
-  if (!player)
-  {
+/**This function returns the location of the player */
+Id player_get_location(Player *player){
+  if (!player){
     return NO_ID;
   }
   return player->location;
 }
 
-Status player_print(Player *player)
-{
+/**This function prints the details of the player,
+ * including the ID, name, location, health, and backpack contents */
+Status player_print(Player *player){
 
   /* Error Control */
-  if (!player)
-  {
+  if (!player){
     return ERROR;
   }
 
-  /* 1. Print the id and the name of the player */
+  /* Print the id and the name of the player */
   fprintf(stdout, "--> Player (Id: %ld; Name: %s)\n", player->id, player->name);
-  if (player->object == NO_ID)
-  {
-    fprintf(stdout, "--> The player does not have an object");
-  }
-  else
-  {
-    fprintf(stdout, "--> Id of the player's object: %ld", player->object);
-  }
+
+  /* Print the player's location */
   fprintf(stdout, "--> Id of the player's location: %ld", player->location);
+
+  /* Print the player's health */
+  fprintf(stdout, "--> Player's health: %d\n", player->health);
+
+  /* Print the backpack details */
+  if (player->backpack != NULL){
+    inventory_print(player->backpack);
+  } else {
+    fprintf(stdout, "--> The player does not have a backpack\n");
+  }
 
   return OK;
 }
 
-Status player_set_health(Player *p, int hp)
-{
+Status player_set_health(Player *p, int hp){
   if (!p)
     return ERROR;
   p->health = hp;
   return OK;
 }
 
-int player_get_health(Player *p)
-{
+int player_get_health(Player *p){
   if (!p)
     return NO_HP;
   return p->health;
