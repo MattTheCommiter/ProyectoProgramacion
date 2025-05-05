36c57941e27d26af23b20008b0a1ad71c1086e35
Matteo <matteo.artu@gmail.com>
2025-05-05 23:47:08 +0200

    Nuevos espacios en el .dat

--- include/cinematics.h
+++ include/cinematics.h
@@ -16,6 +16,7 @@
 
 /**
  * @brief Data structure for managing the cinematics of the game
+ * @author Matteo Artunedo
  */
 typedef enum {
     INTRODUCTION,   /*!<Corresponds to the cinematic that will be played at the beggining of the game*/
@@ -27,29 +28,30 @@ typedef enum {
 #define N_CINEMATICS NO_CINEMATIC   /*!<Value that describes the number of cinematics in the game*/
 #define NUMBER_OF_DIALOG_LINES 20   /*!<Corresponds to the maximum number of interventions that can take place in a cinematic*/
 #define DIALOGUE_LINE_LENGTH 180      /*!<Corresponds to the maximum length of each intervention*/
+#define TIME_BETWEEN_CINEMATICS 1  /*!< Ammount of seconds the game gives the player to read each dialogue line in a cinematic*/
 /**
  * @brief Definition of Cinematics_text type
- * 
+ * @author Matteo Artunedo
  */
 typedef struct _Cinematics_text Cinematics_text;
 
 /**
  * @brief Creates a new Cinematics_text structure to store the dialogue of a certain cinematic
- * 
+ * @author Matteo Artunedo
  * @return pointer to the created structure
  */
 Cinematics_text *cinematics_text_create();
 
 /**
  * @brief frees the memory of a Cinematics_text structure
- * 
+ * @author Matteo Artunedo
  * @param ct pointer to the structure
  */
 void cinematics_text_destroy(Cinematics_text *ct);
 
 /**
  * @brief adds a dialog line to a certain cinematics_text structure
- * 
+ * @author Matteo Artunedo
  * @param ct the structure corresponding to the cinematic
  * @param line the line to be added
  * @return Status OK or ERROR
@@ -58,7 +60,7 @@ Status cinematics_text_add_line(Cinematics_text *ct, char *line);
 
 /**
  * @brief returns the dialog line from a cinematic in a certain position
- * 
+ * @author Matteo Artunedo
  * @param ct pointer to the structure that stores the cinematic's dialogue
  * @param pos position of the line in the cinematic
  * @return char* the line in the given position 
@@ -67,10 +69,18 @@ char *cinematics_get_line(Cinematics_text *ct, int pos);
 
 /**
  * @brief gets the number of lines in a cinematic's dialogue
- * 
+ * @author Matteo Artunedo
  * @param ct pointer to the structure that stores the cinematic's text
  * @return int number of dialogue lines in the cinematic
  */
 int cinematics_get_n_lines(Cinematics_text *ct);
+
+/**
+ * @brief Runs the cinematic currently present in the game through the given graphic engine
+ * @author Matteo Artunedo
+ * @param game pointer to the game
+ * @param gengine pointer to the graphic engine that runs the game
+ */
+/*void cinematics_run(Game *game, Graphic_engine *gengine);*/
 
 #endif
